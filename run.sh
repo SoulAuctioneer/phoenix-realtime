bbbf162596eab0838977c2727bb929dc155428e8
SoulAuctioneer <ash.eldritch@gmail.com>
2025-01-15 18:59:03 -0800

    cleaner startup

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
-# Activate the virtual environment
-source venv/bin/activate
-
 # Load environment variables from .env file
 set -a
 source local.env
 set +a
+
+# Activate the virtual environment only if not already active
+if [ -z "$VIRTUAL_ENV" ]; then
+    source venv/bin/activate
+fi
 
 # Run the main.py file
 python3 src/main.py
